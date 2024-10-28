bce5710e12b219a337be044b2fda102cb6c2ba86
Adam Horvath <adam.horvath16299@gmail.com>
2024-10-28 21:42:21 +0100

    update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,18 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, varchar, integer, pgEnum, uniqueIndex } from "drizzle-orm/pg-core";
 
-export const User = pgTable("users", {
-  id: serial("id").primaryKey(),
-  fullName: text("full_name"),
+export const userPermissions = pgEnum("userPermissions", ['ADMIN', 'BASIC'])
+
+export const User = pgTable("user", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 256 }).notNull(),
+  email: varchar("email", { length: 256 }).notNull(),
+  age: integer("age").notNull(),
+  first_name: text("first_name"),
+  second_name: text("second_name"),
   phone: varchar("phone", { length: 256 }),
+}, table => {
+  return{
+    emailIndex: uniqueIndex("emailIndex").on(table.email),
+    nameIndex: uniqueIndex("uniqueNameAndAge").on(table.name)
+  }
 });
